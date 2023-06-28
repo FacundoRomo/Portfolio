a3cb1830ad94499fa5b3519db56c4ac9920596df
Facundo Romo <facuuromo216@gmail.com>
2023-06-28 11:18:20 -0300

    Updates

---
+++ assets/proyect.js
@@ -0,0 +1,26 @@
+export const PROYECTS = [
+  {
+    img: '/assets/proyects/proyect1.jpg',
+    title: 'Ecommerce',
+    descrp: 'Tienda online donde se podra ver productos de todos tipos, que se pueden filtrar mediante el precio como la catagoria y ademas se pueden agregar a un carrito el producto que desee',
+    url: 'https://facundoromo.github.io/Shop/'
+  },
+  {
+    img: '/assets/proyects/proyect2 (2).jpg',
+    title: 'Ta te ti',
+    descrp: 'Un juego tan simple como divertido',
+    url: 'https://facundoromo.github.io/tateti/'
+  },
+  {
+    img: '/assets/proyects/proyect3.jpg',
+    title: 'Administrador de tareas',
+    descrp: 'Un elegante, simple y funcional anotador, donde se puede agregar tachar y eliminar las notas',
+    url: 'https://facundoromo.github.io/AdministradorDeTareas/'
+  },
+  {
+    img: '/assets/proyects/proyect4.jpg',
+    url: 'https://facundoromo.github.io/Calculadora/',
+    title: 'Calculadora',
+    descrp: 'Una calculadora elegante con las funciones basicas'
+  }
+]
